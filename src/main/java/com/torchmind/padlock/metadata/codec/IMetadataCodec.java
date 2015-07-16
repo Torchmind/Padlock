08ac49cd872813a21b1c71d400b065ddc2f2d92d
Johannes Donath <johannesd@torchmind.com>
2015-07-16 23:45:03 +0200

    Moved to appropriate package

--- src/main/java/com/torchmind/padlock/metadata/codec/IMetadataCodec.java
+++ src/main/java/com/torchmind/padlock/metadata/codec/IMetadataCodec.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.torchmind.padlock.metadata;
+package com.torchmind.padlock.metadata.codec;
 
 import com.torchmind.padlock.metadata.AuthenticationClaimMetadata;
 
